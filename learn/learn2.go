8408dfa5ba04aed4fd365db716c1ad430d602ab2
bubble <1270904398@qq.com>
2024-09-25 23:25:12 +0800

    day2

---
+++ learn/learn2.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func Out() {
+	fmt.Println("hello world" + "lxz")
+
+}
--- learn/main.go
+++ learn/main.go
@@ -8,10 +8,12 @@ import (
 
 func main() {
 	var s, sep string
-	fmt.Println(os.Args[0])
+	// fmt.Println(os.Args[0])
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
+
+	Out()
 }
